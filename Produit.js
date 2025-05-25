86b7f32efe7de1b9fb6c1da3dad9612ca07a13c0
MarieHPSL <hermine-marie.durand@psl.eu>
2025-05-25 18:35:40 +0200

    Create Produit.js

---
+++ Produit.js
@@ -0,0 +1,9 @@
+document.getElementById("envoie").addEventListener("click", function () {
+  alert("Merci, vos critères ont bien été enregistrés💙");
+});
+
+document
+  .getElementById("Ajouteraupanier")
+  .addEventListener("click", function () {
+    alert("Merci, votre commande a bien été enregistrée💙");
+  });
